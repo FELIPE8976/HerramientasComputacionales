a15ad3edb851dbe1ba5f9d13f28ec181bd1b76a4
FELIPE8976 <85198296+FELIPE8976@users.noreply.github.com>
2021-06-17 14:54:48 -0500

    Delete cambio moneda.py

--- cambio moneda.py
+++
@@ -1,9 +0,0 @@
-cop = eval(input("Ingrese el valor a cambiar en pesos colombianos: "))
-usd = cop * 0.00028
-yen = cop * 0.030
-euro = cop * 0.00023
-t1 =  usd - (usd * 0.02) 
-t2 = yen - (yen * 0.02) 
-t3 = euro - (euro * 0.02)
-lista = [t1,t2,t3]
-print("El valor que se le entregara en usd, yen o euro es respectivamente: "  + str(lista))
