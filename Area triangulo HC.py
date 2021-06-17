063dc4553eeb97516cdeb78191baa78b60ee468e
FELIPE8976 <85198296+FELIPE8976@users.noreply.github.com>
2021-06-17 11:22:46 -0500

    Delete Area triangulo HC.py

--- Area triangulo HC.py
+++
@@ -1,4 +0,0 @@
-base = eval(input("Ingrese el valor de la base del triangulo en cm: "))
-altura = eval(input("Ingrese el valor de la altura en cm: "))
-area = (base * altura) /2
-print("El area del triangulo es: " + str(area) + " cm")
